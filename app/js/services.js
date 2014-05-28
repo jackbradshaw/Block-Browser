c1967e24a320b6bc903795aa4dc3353ed7dfcff4
unknown <jbradshaw@SL-WS-212.scottlogic.co.uk>
2014-05-28 16:03:41 +0100

    Refactored block chain promises to be iterative and not use array in closure

--- app/js/services.js
+++ app/js/services.js
@@ -8,21 +8,53 @@
 var blockBrowserServices = angular.module('blockBrowser.services', ['ngResource']);
   
   
-blockBrowserServices.factory('Blocks', ['$resource', '$http',
-  function($resource, $http){
+blockBrowserServices.factory('Blocks', ['$resource', '$http', '$q',
+  function($resource, $http, $q){
   
 	return new BlockService();
 	
 	function BlockService()
 	{
-		this.resource =  $resource('/rawblock/:block_hash', {}, {
-		  //query: {method:'GET', url:'https://blockchain.info/blocks/:time_in_milliseconds?format=json&cors=true', isArray:true}
+		this.resource =  $resource('/rawblock/:block_hash', {}, {		  
 		  query: {method:'GET', url:'/blocks/:time_in_milliseconds?format=json&cors=true'}
-		});
-		
+		});		
 		
 		this.blockChain = function(blocksToGet, startingBlockHash)
 		{	
+			var promise;
+			if(startingBlockHash == null)
+			{
+				promise = $http({method:'GET', url: '/q/latesthash'})
+					
+			}else
+			{
+				promise = $q.when(undefined);	
+			}
+			
+			promise = promise.then(function(result) { 						
+						return {
+							nextHash: result.data === undefined ? startingBlockHash : result.data,
+							blocks: []
+						};
+					});	
+			
+			for (var i = 0; i < blocksToGet; i++) {
+				promise = promise.then(function(obj) { 
+						return $http({method:'GET', url: '/rawblock/' + obj.nextHash})
+							.then(function(result) {
+								obj.blocks.push(result.data);
+								obj.nextHash = result.data.prev_block;
+								return obj;
+							});
+					});
+			}
+			
+			return promise.then(function(obj) {
+				return obj.blocks;
+			});
+
+
+			
 			if(!startingBlockHash)
 			{
 				var hashPromise = $http({method:'GET', url: '/q/latesthash'});
@@ -32,21 +64,27 @@ blockBrowserServices.factory('Blocks', ['$resource', '$http',
 			
 			function promiseChain(blocksToGet, hash)
 			{
-				var blocksLeft = blocksToGet;
+				var blocksLeft = blocksToGet - 1;
 				var blockChain = [];				
 				return $http({method:'GET', url: '/rawblock/' + hash}).then(myThen);
+				
 				function myThen(block)
 				{
+					//Add block to array:
+					blockChain.push(block.data);
+					
+					//If we need to get more blocks add another block promise to the end of the promise chain and recurse:
 					if(blocksLeft-- > 0)
-					{
-						blockChain.push(block.data);
-						return $http({method:'GET', url: '/rawblock/' + block.data.prev_block}).then(myThen);									
-					}else
+					{						
+						return $http({method:'GET', url: '/rawblock/' + block.data.prev_block}).then(myThen);										
+					}
+					//The final promise on the chain returns the full array:
+					else
 					{
 						return blockChain;
 					}
 				}
-			};						
+			};		
 		};
 	}	
   }]);
